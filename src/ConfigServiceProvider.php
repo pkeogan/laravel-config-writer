472d1fbd48db3489d562fb459556d9ea27e76706
Peter Keogan <30511153+pkeogan@users.noreply.github.com>
2018-09-05 11:53:57 -0500

    init

---
+++ src/ConfigServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pkeogan\LaravelConfigWriter;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConfigServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind it only once so we can reuse in IoC
+        $this->app->singleton('configwriter', function($app, $items)
+        {
+            $writer = new FileWriter($app['files'], $app['path.config']);
+            return new Repository($items, $writer);
+        });
+
+        // Capture the loaded configuration items
+        $config_items = app('config')->all();
+
+        $this->app['config'] = $this->app->share(function($app) use ($config_items)
+        {
+            return $app->make('configwriter', $config_items);
+        });
+    }
+}
